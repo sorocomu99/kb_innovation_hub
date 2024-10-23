49708bcc390f9f28745e4fd89f0c92723a74be9a
프로 <@DESKTOP-T8HA1B0>
2024-10-23 14:54:00 +0900

    우측 공백 제거 함수 추가

--- src/main/java/com/kb/inno/common/StringUtil.java
+++ src/main/java/com/kb/inno/common/StringUtil.java
@@ -3,6 +3,12 @@ package com.kb.inno.common;
 import org.springframework.util.StringUtils;
 
 public class StringUtil extends StringUtils {
+	
+	/**
+	 * 문자열에 공백이 들어온 경우 "0"으로 리턴하는 메소드
+	 * @param value
+	 * @return value
+	 */
     public static String nullToZero(String value) {
         if (value == null || value.equals("")) {
             value = "0";
@@ -11,18 +17,33 @@ public class StringUtil extends StringUtils {
         return value;
     }
 
-    /**  * 문자열 좌측의 공백을 제거하는 메소드 
+    /**  
+     * 문자열 좌측의 공백을 제거하는 메소드 
      * @param  str 대상 문자열 :
      * @return trimed string with white space removed from the front. 
      */
     public static String ltrim(String str) {
         int len = str.length();
         int idx = 0;
-        while ((idx < len) && (str.charAt(idx) <= ' '))
-        {
+        while ((idx < len) && (str.charAt(idx) <= ' ')) {
             idx++;
         }
 
         return str.substring(idx, len);
+    }
+    
+    /**
+     * 문자열 우측의 공백을 제거하는 메소드
+     * @param  str 대상 문자열
+     * @return trimed string with white space removed from the end
+     */
+    public static String rtrim(String str) {
+    	int len = str.length();
+    	
+    	while ((0 < len) && str.charAt(len - 1) <= ' ') {
+    		len--;
+    	}
+    	
+    	return str.substring(0, len);
     }
 }
