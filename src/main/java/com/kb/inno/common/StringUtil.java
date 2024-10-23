6b844bb9cd46678620f1bfe6fcd66ed3743df09a
hunhee <hunhee@soroweb.co.kr>
2024-10-23 14:39:41 +0900

    문자열 공통함수 최초작성

---
+++ src/main/java/com/kb/inno/common/StringUtil.java
@@ -0,0 +1,30 @@
+package com.kb.inno.common;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import org.apache.commons.lang.StringUtils;
+
+public class StringUtil extends StringUtils {
+    public static String nullToZero(String value) {
+        if (value == null || value.equals("")) {
+            value = "0";
+        }
+
+        return value;
+    }
+
+    /**  * 문자열 좌측의 공백을 제거하는 메소드 
+     * @param  str 대상 문자열 :
+     * @return trimed string with white space removed from the front. 
+     */
+    public static String ltrim(String str) {
+        int len = str.length();
+        int idx = 0;
+        while ((idx < len) && (str.charAt(idx) <= ' '))
+        {
+            idx++;
+        }
+
+        return str.substring(idx, len);
+    }
+}
