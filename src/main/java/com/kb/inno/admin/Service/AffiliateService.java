f3dd6d087233c333dcbdd4a5a319548f274af5a5
DEV <yunji1994@soroweb.co.kr>
2024-11-22 10:52:37 +0900

    제휴 사례 관리 fileupload 수정 및 validation check

--- src/main/java/com/kb/inno/admin/Service/AffiliateService.java
+++ src/main/java/com/kb/inno/admin/Service/AffiliateService.java
@@ -14,6 +14,7 @@ import com.kb.inno.admin.DAO.AffiliateDAO;
 import com.kb.inno.admin.DTO.AffiliateDTO;
 import com.kb.inno.admin.DTO.FileDTO;
 import com.kb.inno.admin.DTO.VisualDTO;
+import com.kb.inno.common.FileUploader;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -35,71 +36,9 @@ public class AffiliateService {
     // DAO 연결
     private final AffiliateDAO affiliateDAO;
 
-    // 파일 경로
-    @Value("src/main/resources/static/")
-    private String staticPath;
-
     // 제휴 사례 리스트 조회
     public List<AffiliateDTO> selectList() {
         return affiliateDAO.selectList();
-    }
-
-    // 파일 저장
-    public FileDTO insertFile(AffiliateDTO affiliateDTO, int loginId) {
-        // 파일 꺼내기
-        MultipartFile file = affiliateDTO.getAffiliate_file();
-
-        // 오리지널 파일 이름
-        String originalFilename = file.getOriginalFilename();
-
-        // 확장자 꺼내기
-        String fileExtension = originalFilename.substring(originalFilename.lastIndexOf("."));
-
-        // 파일 이름 설정
-        String fileName = UUID.randomUUID().toString() + fileExtension;
-
-        // 파일 경로 설정
-        Path path = Paths.get(System.getProperty("user.dir"), staticPath);
-        String savePath = path + "\\upload\\";
-
-        // 파일 디렉토리 생성(없으면)
-        File directory = new File(savePath);
-        if (!directory.exists()) {
-            directory.mkdirs();
-        }
-
-        try {
-            file.transferTo(new File(savePath, fileName));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        // 파일 사이즈 구하기
-        int bytes = (int) file.getSize();
-
-        // 빈 DTO 생성
-        FileDTO fileDTO = new FileDTO();
-
-        // DTO에 객체 담기
-        fileDTO.setFile_nm(fileName);
-        fileDTO.setOrgnl_file_nm(originalFilename);
-        fileDTO.setFile_extn(fileExtension);
-        fileDTO.setFile_path("\\upload\\");
-        fileDTO.setFile_sz(bytes);
-
-        // 파일 DTO에 로그인 한 사람 추가
-        fileDTO.setFrst_rgtr(loginId);
-        fileDTO.setLast_mdfr(loginId);
-
-        // 파일 테이블 저장
-        int result = affiliateDAO.insertFile(fileDTO);
-
-        // 결과가 있으면 fileDTO return
-        if(result == 1) {
-            return fileDTO;
-        } else {
-            return null;
-        }
     }
 
     // 제휴 사례 등록
@@ -113,10 +52,16 @@ public class AffiliateService {
 
         if(fileYn == 1) {
             // 파일 저장
-            FileDTO fileSave = insertFile(affiliateDTO, loginId);
+            MultipartFile file = affiliateDTO.getAffiliate_file();
+
+            FileUploader fileUploader = new FileUploader();
+            FileDTO fileSave = fileUploader.insertFile(file, loginId);
+
+            // 파일 테이블에 저장
+            int result = affiliateDAO.insertFile(fileSave);
 
             // 게시글 저장
-            if(fileSave != null) {
+            if(fileSave != null && result == 1) {
                 // 파일 일련번호 대입
                 affiliateDTO.setAtch_file_sn(fileSave.getFile_sn());
             }
@@ -142,22 +87,23 @@ public class AffiliateService {
             AffiliateDTO basicFile = affiliateDAO.select(affiliateDTO.getAffiliate_sn());
 
             // 1. 기존 경로에 있는 파일 삭제
-            // 경로 설정
-            Path path = Paths.get(System.getProperty("user.dir"), staticPath);
-            File deleteFile = new File(path + basicFile.getAffiliate_path() + basicFile.getAffiliate_file_name());
-            boolean removed = deleteFile.delete();
+            FileUploader fileUploader = new FileUploader();
+            boolean removed = fileUploader.deleteFile(basicFile.getAffiliate_path(), basicFile.getAffiliate_file_name());
 
-            // 2. 만약 경로에 파일이 지워졌다면
+            // 2. 만약 경로에 파일이 지워졌다면 테이블에 있는 파일 삭제
             if(removed) {
-                // 테이블에 있는 파일 삭제
                 affiliateDAO.deleteFile(basicFile.getAtch_file_sn());
             }
 
             // 3. 새로운 파일 경로에 저장
-            FileDTO fileSave = insertFile(affiliateDTO, loginId);
+            MultipartFile file = affiliateDTO.getAffiliate_file();
+            FileDTO fileSave = fileUploader.insertFile(file, loginId);
+
+            // 파일 테이블에 저장
+            int result = affiliateDAO.insertFile(fileSave);
 
             // 4. 새로운 파일 테이블에 저장
-            if(fileSave != null) {
+            if(fileSave != null && result == 1) {
                 // 파일 일련번호 대입
                 affiliateDTO.setAtch_file_sn(fileSave.getFile_sn());
             }
@@ -180,9 +126,8 @@ public class AffiliateService {
         // 2. fileId가 빈 값이 아니면(파일이 있으면)
         if(file_sn != 0) {
             // 경로 내 파일 삭제
-            Path path = Paths.get(System.getProperty("user.dir"), staticPath);
-            File deleteFile = new File(path + selectInfo.getAffiliate_path() + selectInfo.getAffiliate_file_name());
-            boolean removed = deleteFile.delete();
+            FileUploader fileUploader = new FileUploader();
+            boolean removed = fileUploader.deleteFile(selectInfo.getAffiliate_path(), selectInfo.getAffiliate_file_name());
 
             // 만약 경로에 파일이 지워졌다면
             if(removed) {
@@ -191,7 +136,7 @@ public class AffiliateService {
             }
         }
 
-        // 제휴 사례 삭제
+        // 3. 제휴 사례 삭제
         affiliateDAO.delete(affiliate_sn);
     }
 }
