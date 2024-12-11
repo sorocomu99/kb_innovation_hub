62255bd8e615816cfd3209ba6ca18be8a5eec76c
DEV <yunji1994@soroweb.co.kr>
2024-12-11 10:38:13 +0900

    콤마 추가

--- src/main/java/com/kb/inno/admin/DTO/ResultDTO.java
+++ src/main/java/com/kb/inno/admin/DTO/ResultDTO.java
@@ -17,11 +17,11 @@ import java.util.Date;
 @Data
 public class ResultDTO {
     private int rslt_sn;                // 성과 일련 번호
-    private int ent_nocs;               // 기업 건수(육성기업)
+    private String ent_nocs;            // 기업 건수(육성기업)
     private String ent_crtr_ymd;        // 기준년월일(육성기업)
-    private int invest_nocs;            // 투자 건수
+    private String invest_nocs;         // 투자 건수
     private String invest_crtr_ymd;     // 투자 기준년월일
-    private int affiliate_nocs;         // 제휴 건수
+    private String affiliate_nocs;      // 제휴 건수
     private String affiliate_crtr_ymd;  // 제휴 기준년월일
     private int frst_rgtr;              // 최초 등록자
     private Date frst_reg_dt;           // 최초 등록 일시
