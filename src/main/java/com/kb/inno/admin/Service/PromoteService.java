582be57c86e3991d70d6b4e6596026f766e50a94
DEV <yunji1994@soroweb.co.kr>
2024-12-11 11:16:38 +0900

    콤마 추가 저장하기 수정

--- src/main/java/com/kb/inno/admin/Service/PromoteService.java
+++ src/main/java/com/kb/inno/admin/Service/PromoteService.java
@@ -29,6 +29,25 @@ public class PromoteService {
 
     // 국내 프로그램 - 육성 그래프 등록
     public int insert(PromoteDTO promoteDTO, int loginId) {
+        // , 제거
+        String graph1_nocs = promoteDTO.getGraph1_nocs();
+        String graph2_nocs = promoteDTO.getGraph2_nocs();
+        String graph3_nocs = promoteDTO.getGraph3_nocs();
+        String graph4_nocs = promoteDTO.getGraph4_nocs();
+        String graph5_nocs = promoteDTO.getGraph5_nocs();
+
+        String graph1 = graph1_nocs.replaceAll(",", "");
+        String graph2 = graph2_nocs.replaceAll(",", "");
+        String graph3 = graph3_nocs.replaceAll(",", "");
+        String graph4 = graph4_nocs.replaceAll(",", "");
+        String graph5 = graph5_nocs.replaceAll(",", "");
+
+        promoteDTO.setGraph1_nocs(graph1);
+        promoteDTO.setGraph2_nocs(graph2);
+        promoteDTO.setGraph3_nocs(graph3);
+        promoteDTO.setGraph4_nocs(graph4);
+        promoteDTO.setGraph5_nocs(graph5);
+
         // 로그인 한 아이디 대입
         promoteDTO.setFrst_rgtr(loginId);
         promoteDTO.setLast_mdfr(loginId);
@@ -38,6 +57,25 @@ public class PromoteService {
 
     // 국내 프로그램 - 육성 그래프 수정
     public int update(PromoteDTO promoteDTO, int loginId) {
+        // , 제거
+        String graph1_nocs = promoteDTO.getGraph1_nocs();
+        String graph2_nocs = promoteDTO.getGraph2_nocs();
+        String graph3_nocs = promoteDTO.getGraph3_nocs();
+        String graph4_nocs = promoteDTO.getGraph4_nocs();
+        String graph5_nocs = promoteDTO.getGraph5_nocs();
+
+        String graph1 = graph1_nocs.replaceAll(",", "");
+        String graph2 = graph2_nocs.replaceAll(",", "");
+        String graph3 = graph3_nocs.replaceAll(",", "");
+        String graph4 = graph4_nocs.replaceAll(",", "");
+        String graph5 = graph5_nocs.replaceAll(",", "");
+
+        promoteDTO.setGraph1_nocs(graph1);
+        promoteDTO.setGraph2_nocs(graph2);
+        promoteDTO.setGraph3_nocs(graph3);
+        promoteDTO.setGraph4_nocs(graph4);
+        promoteDTO.setGraph5_nocs(graph5);
+
         // 로그인 한 아이디 대입
         promoteDTO.setLast_mdfr(loginId);
 
